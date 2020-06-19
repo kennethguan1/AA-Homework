6b0034cb5364c7bc5ad01ccdcfeec439acab39c5
kennethguan1 <kennethguan1@gmail.com>
2020-06-19 07:54:35 -0700

    new commit

---
+++ W3D5/DIY ADT.rb
@@ -0,0 +1,52 @@
+class Stack
+    def initialize
+      # create ivar to store stack here!
+    end
+
+    def push(el)
+      # adds an element to the stack
+    end
+
+    def pop
+      # removes one element from the stack
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
+
+  new_stack = Stack.new
+
+class Queue
+    def initialize
+    end
+
+    def enqueue(el)
+    end
+
+    def dequeue
+    end
+
+    def peek
+    end
+end
+
+class Map
+    def initialize
+      @map = []
+    end
+
+    def set(key, value)
+    end
+
+    def get(key)
+    end
+
+    def delete(key)
+    end
+
+    def show
+        @map
+    end 
+end
